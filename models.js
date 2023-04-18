88243f2d73c9b127a057592f3fdcf4bf52bb9d5a
Diana0880 <130804501+Diana0880@users.noreply.github.com>
2023-04-18 15:03:13 +0300

    Add files via upload

---
+++ models.js
@@ -0,0 +1,21 @@
+<script>
+var carModels = {
+  "audi": ["Выберете модель", "A3", "A4", "A6"],
+  "bmw": ["", "3 Series", "5 Series", "7 Series"],
+  "mercedes": ["", "C-Class", "E-Class", "S-Class"]
+};
+
+$('#carBrandSelect').change(function(){
+    var selectedBrand = $(this).val();
+    var availableModels = carModels[selectedBrand];
+    updateCarModelSelect(availableModels);
+});
+
+function updateCarModelSelect(availableModels) {
+    var carModelDropdown = $('#carModelSelect');
+    carModelDropdown.empty();
+    $.each(availableModels, function(index, model){
+        carModelDropdown.append('<option value="'+model+'">'+model+'</option>');
+    });
+}
+</script>
